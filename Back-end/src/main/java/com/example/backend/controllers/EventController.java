3568e366c21055a2fd3f64bd80274481bd9a7fba
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-04-30 17:41:46 +0700

    run 3 container

--- Back-end/src/main/java/com/example/backend/controllers/EventController.java
+++ Back-end/src/main/java/com/example/backend/controllers/EventController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "*")
 public class EventController{
     @Autowired
     private EventRepository repository;
