8fe2237628c7c34168e49b978129a5eafa5af3f3
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-05-01 17:14:16 +0700

    edit frontend

--- Front-end/src/views/ViewEvent.vue
+++ Front-end/src/views/ViewEvent.vue
@@ -2,12 +2,21 @@
   import ShowListEvent from '../components/ShowListEvent.vue'
   import {ref, onMounted} from 'vue'
 
-  const events = ref([])
+  const events = ref({})
+  const loading =ref()
+  const message = ref()
 
   const getEvents = async () => {
+    loading.value = true
+    message.value = "loading..."
     // const res = await fetch('http://localhost:8080/api/events')
     const res = await fetch(`${import.meta.env.VITE_BASE_URL}/events`)
+      .catch(()=> {
+        message.value = "Not Found Backend Server!!!"
+    });
     events.value = await res.json()
+    console.log(res.status)
+    loading.value = false
   }
 
   onMounted(async () => {
@@ -18,9 +27,16 @@
 </script>
  
 <template>
-  <ShowListEvent :events="events"/>
+  <div v-if="loading" class="subText">{{message}}</div>
+  <div v-else><ShowListEvent :events="events"/></div>
 </template>
  
 <style>
- 
+ .subText{
+   color: gray;
+   text-align: center;
+ }
+ .centerText{
+   text-align: center;
+ }
 </style>
