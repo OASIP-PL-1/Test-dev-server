b6a8a5f888ad143c41335d50efadafe56c65c3d3
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-05-02 19:04:09 +0700

    close server

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    server: {
+    // server: {
         // proxy: {
         //     "^/api": {
         //         target: "http://10.4.56.100:8080/api",
@@ -15,5 +15,5 @@ export default defineConfig({
         //     }
         // },
         port : 80
-    }
+    // }
 })
