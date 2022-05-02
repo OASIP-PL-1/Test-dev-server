8cdffa0d4f664d4a860d2dae73ab12eed564e272
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-05-02 15:40:12 +0700

    proxy api

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [vue()]
   ,
   server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://10.4.84.106:8080',
-  //       changeOrigin: true,
-  //       secure: false
-  //     }
-    // } 
+    proxy: {
+      '/api': {
+        target: 'http://10.4.84.106:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    } ,
     port : 80
   }
 })
