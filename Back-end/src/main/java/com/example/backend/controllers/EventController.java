3f2f1a0b90555954c0f6b24d7b9b556c1bde84bf
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-05-02 11:50:02 +0700

    port 80 80 proxy

--- Back-end/src/main/java/com/example/backend/controllers/EventController.java
+++ Back-end/src/main/java/com/example/backend/controllers/EventController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 public class EventController{
     @Autowired
     private EventRepository repository;
