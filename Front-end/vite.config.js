2d429b92f0e4f29b441bee4b167bda0cdba4735a
Vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-05-05 14:29:08 +0700

    server proxy

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -4,16 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()]
-    // server: {
-        // proxy: {
-        //     "^/api": {
-        //         target: "http://10.4.56.100:8080/api",
-        //         changeOrigin: true,
-        //         secure: false,
-        //         rewrite: (path) => path.replace(/^\/api/, ''),
-        //         ws: true,
-        //     }
-        // },
-        // port : 80
-    // }
+    ,
+    server: {
+        proxy: {
+            "^/api": {
+                target: "http://10.4.56.100:8080/api",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+                ws: true,
+            }
+        },
+        port : 80
+    }
 })
