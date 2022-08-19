e117b0cbb6bf6e9bd902d4684de706b236aedf45
Cartoonydesu <toon.pi45@gmail.com>
2022-08-19 11:49:07 +0700

    Validation using hashmap : should multiple field but no error code and timestamp

--- Back-end/src/main/java/com/example/backend/dtos/EventCategoryAllDTO.java
+++ Back-end/src/main/java/com/example/backend/dtos/EventCategoryAllDTO.java
@@ -5,13 +5,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.*;
+
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventCategoryAllDTO {
+    @Min(value = 1, message = "The least id number is 1.")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @NotBlank(message = "Event category's name is required.")
     private String categoryName;
+
+    @NotBlank(message = "Event category's description is required.")
     private String categoryDescription;
+
+    @Min(value = 20, message = "Duration must greater than 1.")
+    @NotNull(message = "Duration is required.")
     private int duration;
 }
