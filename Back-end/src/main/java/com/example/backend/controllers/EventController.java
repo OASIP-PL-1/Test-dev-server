d581d76e0581ae557fdb0cf7625cb1295fef2021
vichuta <vichuta.nice@kmutt.ac.th>
2022-10-28 11:55:41 +0700

    New push

--- Back-end/src/main/java/com/example/backend/controllers/EventController.java
+++ Back-end/src/main/java/com/example/backend/controllers/EventController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 public class EventController {
     @Autowired
     private EventService service;
