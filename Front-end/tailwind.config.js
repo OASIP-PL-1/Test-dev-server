ca93b215a11dcfeb50647ddb193ea65bc2b06500
vichuta <vichuta.nice@kmutt.ac.th>
2022-11-28 17:12:12 +0700

    add tailwind

---
+++ Front-end/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+      'K2D': ['K2D', 'sans-serif'] 
+    },},
+  },
+  plugins: [],
+}
